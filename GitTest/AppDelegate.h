7148a3c569ad3c47aac184dbd9ef32d7bd1a2b24
liurihua <liurihua@bytedance.com>
2016-05-30 20:44:24 +0800

    nsl

---
+++ GitTest/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  GitTest
+//
+//  Created by Liu RiHua on 16/5/30.
+//  Copyright © 2016年 Liu RiHua. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
